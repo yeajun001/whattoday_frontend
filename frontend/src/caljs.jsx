3db90ddf352ef1fea0f3e61615c7c1cf8fe32751
unknown <sky210120@daum.net>
2024-07-05 23:04:08 +0900

    cloud test 16

--- frontend/src/caljs.jsx
+++ frontend/src/caljs.jsx
@@ -267,7 +267,7 @@ const Calendar = () => {
         
         const fullImagePath = response.data.imagePath;
         // 도메인과 경로를 합쳐서 이미지 src를 만듭니다.
-        const imageSrc = `https://uploadsdiaryimg.kr.object.ncloudstorage.com/post/${fullImagePath}`;
+        const imageSrc = `/post/${fullImagePath}`;
         console.log(imageSrc);
         setImageSrc(imageSrc);
     } catch (error) {
