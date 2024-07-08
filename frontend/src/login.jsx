f79229a35e0bd364a6e578f6c91d9483cbc6d6f0
unknown <sky210120@daum.net>
2024-07-09 01:05:43 +0900

    login test

--- frontend/src/login.jsx
+++ frontend/src/login.jsx
@@ -17,6 +17,9 @@ const SocialLogin = () => {
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
   const loginWithGoogle = async () => {
     const provider = new GoogleAuthProvider();
     try {
@@ -72,14 +75,50 @@ const SocialLogin = () => {
     });
   }, []);
 
+  const handleLogin = async () => {
+    try {
+      const response = await fetch('https://port-0-whattoday-deploy-backend-ly7hfh5b552425a2.sel5.cloudtype.app/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+      });
+
+      if (response.ok) {
+        // 로그인 성공 처리
+        navigate('/mainlin');
+      } else {
+        // 로그인 실패 처리
+        alert('이메일 또는 비밀번호가 잘못되었습니다.');
+      }
+    } catch (error) {
+      console.error('로그인 요청 실패:', error);
+      alert('서버 오류');
+    }
+  };
+
+
   return (
   <div className={styles["main-login-text-box-right"]}>
     <div className={styles["login-input-box"]}>
-      <input type="text" className={styles["login-input"]} placeholder="이메일" />
-      <input type="password" className={styles["login-input"]} placeholder="비밀번호" />
+    <input
+        type="text"
+        className={styles["login-input"]}
+        placeholder="이메일"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        type="password"
+        className={styles["login-input"]}
+        placeholder="비밀번호"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <div className={styles["button-container"]}>
         <button className={styles["signin-button"]} onClick={() => navigate("/Signin")}>회원가입</button>
-        <button className={styles["login-button"]}>로그인</button>
+        <button className={styles["login-button"]} onClick={handleLogin}>로그인</button>
     </div>
   </div>
   <div className={styles["logo-container"]}>
