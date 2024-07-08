0aa8ff0fa3fdcade0af7acaf48d7e3a114286430
unknown <sky210120@daum.net>
2024-07-08 10:40:35 +0900

    last 3

--- frontend/src/Cal.jsx
+++ frontend/src/Cal.jsx
@@ -126,10 +126,10 @@ function Cal() {
     const handleEat = () => {
       const today = new Date();
       setCurrentYear(today.getFullYear());
-      setCurrentMonth(today.getMonth() + 1);
+      setCurrentMonth(today.getMonth());
       setCurrentDay(today.getDate());
       navigate("/Eat");
-      console.log(today, today.getFullYear(), today.getMonth() + 1, today.getDate());
+      console.log(today, today.getFullYear(), today.getMonth(), today.getDate());
     }
 
     useEffect(() => {
